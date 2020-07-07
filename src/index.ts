3c8f657ddf3053668ea3b5df25d13f12b3730545
ilya <ilya@belsky.in>
2020-07-07 10:41:55 +0200

    Put messages into queue when offline and send them when online

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
 import {Command, flags} from '@oclif/command'
 import * as nodemailer from 'nodemailer'
+import {promises as fs} from 'fs'
+import * as path from 'path'
+import { IConfig } from '@oclif/config'
+import cli from 'cli-ux'
+import Mail = require('nodemailer/lib/mailer')
 
 const omniFocusEmail = 'example@email.com'
 // const myInbox = 'example-2@email.com'
+
+interface Message {
+  subject: string
+  body?: string
+}
 
 class OmnifocusInbox extends Command {
   static description = 'describe the command here'
@@ -15,17 +25,11 @@ class OmnifocusInbox extends Command {
     body: flags.string({char: 'b'}),
   }
 
-  async run() {
-    const {argv, flags} = this.parse(OmnifocusInbox)
-    const subject = argv.join(' ')
-    const body = flags.body
-    if (subject.trim().length === 0) {
-      this.log('Nothing to send')
-      this.exit()
-    }
+  transport!: Mail
 
-    this.log('Sending…')
-    const transport = await nodemailer.createTransport({
+  constructor(argv: string[], command: IConfig) {
+    super(argv, command)
+    this.transport = nodemailer.createTransport({
       host: 'mail.gandi.net',
       secure: true, // use SSL
       port: 465, // port for secure SMTP
@@ -34,16 +38,78 @@ class OmnifocusInbox extends Command {
         pass: 'SecretPassw0rd',
       },
     })
+  }
 
-    await transport.sendMail({
+  async run() {
+    const {argv, flags} = this.parse(OmnifocusInbox)
+    const subject = argv.join(' ')
+    const body = flags.body
+    const hasMessage = subject.trim().length > 0
+    let addToQueue = false
+
+
+    if (hasMessage) {
+      cli.action.start('Sending')
+      await this.send({subject, body})
+      .then(() => cli.action.stop())
+      .catch((error: any) => {
+        if (error.code === 'EDNS') {
+          cli.action.stop('no connection')
+          addToQueue = true
+        }
+      })
+    }
+
+    // process queue
+    //
+    const queuePath = path.resolve(process.env.HOME || '', '.of_queue')
+    let queue: Message[] = JSON.parse(
+      await fs.readFile(queuePath)
+      .then(buff => buff.toString())
+      .catch(() => {
+        this.log('No queue file found', queuePath)
+      }) || '[]'
+    )
+
+    if (addToQueue) {
+      queue.push({subject, body})
+      this.log(`Message added to queue (${queue.length} in total)`)
+    } else if (queue.length) {
+      let sending
+      if(hasMessage) {
+        sending = `There are messages in queue, sending them too`
+      } else {
+        sending = `Sending messages from queue`
+      }
+      const failed: Message[] = []
+      let processed = 0
+      const bar = cli.progress({ format: `${sending} {value}/{total}` })
+      bar.start(queue.length, 0)
+      await Promise.all(queue.map((message: Message, i: number) => this.send(message)
+        .catch((error) => {
+          failed.push(message)
+          if (error.code !== 'EDNS') {
+            this.log('Message was not sent', error)
+          }
+        })
+        .finally(() => bar.update(++processed))
+      ))
+      bar.stop()
+      if (failed.length) {
+        this.log(`I was not able to send ${failed.length} messages. Keeping them in the queue`)
+      }
+      queue = failed
+    }
+    await fs.writeFile(queuePath, JSON.stringify(queue))
+  }
+
+  send({subject, body}: Message) {
+    return this.transport.sendMail({
       from: 'OmniFocus inbox <from@email.com>',
       to: omniFocusEmail,
-      // to: myInbox,
       subject,
       text: body,
     })
-
-    this.log('Done')
   }
 }
 
